622d0f443cde42d9bbfb6ef498989936968586b2
luis311050 <71629276+luis311050@users.noreply.github.com>
2022-02-01 13:36:50 +0100

    projecto

---
+++ My project (1)/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    public float Speed;
+    public AudioClip Sound;
+
+    private Rigidbody2D Rigidbody2D;
+    private Vector3 Direction;
+
+    private void Start()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+        Camera.main.GetComponent<AudioSource>().PlayOneShot(Sound);
+    }
+
+    private void FixedUpdate()
+    {
+        Rigidbody2D.velocity = Direction * Speed;
+    }
+
+    public void SetDirection(Vector3 direction)
+    {
+        Direction = direction;
+    }
+    
+public void DestroyBullet()
+    {
+        Destroy(gameObject);
+    }
+}
